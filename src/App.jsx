52171d6cdbac39679722839b7edd9ff72bb0ba45
Lifad457 <cedric.bison@gmail.com>
2024-01-22 07:51:03 +0100

    Add Tic Tac Toe functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,71 @@
+import { useEffect, useState } from 'react'
 import Grid from './components/Grid'
 import { Container, Title } from './styles/app.css'
 import { GlobalStyles } from './styles/global.css'
+import Circle from "./components/Circle";
+import Cross from "./components/Cross";
 
-function App() {
+export default function App() {
+  const [current, setCurrent] = useState(<Circle />)
+  const [grid, setGrid] = useState([
+      { id: 0, value: null },
+      { id: 1, value: null },
+      { id: 2, value: null },
+      { id: 3, value: null },
+      { id: 4, value: null },
+      { id: 5, value: null },
+      { id: 6, value: null },
+      { id: 7, value: null },
+      { id: 8, value: null },
+  ])
+  const [winner, setWinner] = useState(null)
+
+  useEffect(() => {
+      function checkWinner() {
+          const winningCombos = [
+              [1, 2, 3],
+              [1, 4, 7],
+              [1, 5, 9],
+              [2, 5, 8],
+              [3, 5, 7],
+              [3, 6, 9],
+              [4, 5, 6],
+              [7, 8, 9]
+          ]
+          winningCombos.map((item) => {
+              const [a, b, c] = item
+              if (grid[a-1].value !== null 
+                  && grid[a-1].value?.type.name === grid[b-1].value?.type.name 
+                  && grid[b-1].value?.type.name === grid[c-1].value?.type.name) {
+                  setWinner(grid[a-1].value.type.name)
+              }
+          })
+      }
+      checkWinner()
+  }, [grid])
+
+  function handleClick(e) {
+      const target = e.target.id
+      setGrid(grid.map((item) => {
+          if (item.id == target && !item.value) {
+              setCurrent(current.type.name==='Circle' ? <Cross /> : <Circle />)
+              return {...item, value: current}
+          }
+          return item
+      }))
+  }
+  
   return (
     <>
       <GlobalStyles />
       <Container>
-        <Title>Tic<br/>Tac<br/>Toe</Title>
-        <Grid />
+        { winner ? <Title>{winner} win!</Title> : 
+            <>
+              <Title>Tic<br/>Tac<br/>Toe</Title>
+              <Grid grid={grid} handleClick={handleClick} />
+            </>
+         } 
       </Container>
     </>
   )
 }
-
-export default App
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,63 +1,19 @@
-import { useState } from "react";
 import { GridContainer, StyledGrid } from "../styles/grid.css";
-import Circle from "./Circle";
-import Cross from "./Cross";
 
-export default function Grid() {
-    const [current, setCurrent] = useState(<Circle />)
-    const [grid, setGrid] = useState([
-        { id: 1, value: null },
-        { id: 2, value: null },
-        { id: 3, value: null },
-        { id: 4, value: null },
-        { id: 5, value: null },
-        { id: 6, value: null },
-        { id: 7, value: null },
-        { id: 8, value: null },
-        { id: 9, value: null },
-    ])
 
-    function handleClick(e) {
-        const target = e.target.id
-        setGrid(grid.map((item) => {
-            if (item.id == target && !item.value) {
-                item.value = current
-                setCurrent(current.type.name==='Circle' ? <Cross /> : <Circle />)
-                return item
-            }
-            return item
-        }))
-    }
-    console.log(grid)
+export default function Grid({ grid, handleClick }) {
+    const classList = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    const gridElements = classList.map((item, id) => {
+        return (
+            <StyledGrid key={id} id={id} className={item} onClick={handleClick}>
+                { grid[id].value && grid[id].value}
+            </StyledGrid>
+        )
+    })
+
     return (
         <GridContainer>
-            <StyledGrid id='1' className='one' onClick={handleClick}>
-                { grid[0].value && grid[0].value}
-            </StyledGrid>
-            <StyledGrid id='2' className='two' onClick={handleClick}>
-                { grid[1].value && grid[1].value}
-            </StyledGrid>
-            <StyledGrid id='3' className='three' onClick={handleClick}>
-                { grid[2].value && grid[2].value}
-            </StyledGrid>
-            <StyledGrid id='4' className='four' onClick={handleClick}>
-                { grid[3].value && grid[3].value}
-            </StyledGrid>
-            <StyledGrid id='5' className='five' onClick={handleClick}>
-                { grid[4].value && grid[4].value}
-            </StyledGrid>
-            <StyledGrid id='6' className='six' onClick={handleClick}>
-                { grid[5].value && grid[5].value}
-            </StyledGrid>
-            <StyledGrid id='7' className='seven' onClick={handleClick}>
-                { grid[6].value && grid[6].value}
-            </StyledGrid>
-            <StyledGrid id='8' className='eight' onClick={handleClick}>
-                { grid[7].value && grid[7].value}
-            </StyledGrid>
-            <StyledGrid id='9' className='nine' onClick={handleClick}>
-                { grid[8].value && grid[8].value}
-            </StyledGrid>
+            {gridElements}
         </GridContainer>
     )
 }
